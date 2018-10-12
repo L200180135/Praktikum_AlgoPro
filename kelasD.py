4f739624600f9cb857be7b6be213f76b57932416
L200180135 <44077767+L200180135@users.noreply.github.com>
2018-10-12 09:32:44 +0700

    Praktikum 6
    Praktikum 6

---
+++ kelasD.py
@@ -0,0 +1,33 @@
+##S = 'Aku biasa dipanggil Hiroko'
+##
+##print (S[4:19])
+##print (S[21:])
+##print (S[:3])
+##print (S[:4] + S[10:])
+
+
+###TUPLE
+##A = (2, 7, 19, 11, 5, 13, 3, 17)
+##print (A[4])
+##C = A[1:4]
+##
+##B = (4, 'yes', -21, 44, 'aduh')
+##B[1] = 'no' #immutable
+
+#LIST 
+A = [2, 7, 19, 11, 5, 13, 3, 17]
+print (A[4])
+C = A[1:4]
+
+B = [4, 'yes', -21, 44, 'aduh']
+print (B)
+B[1] = 'no'
+print (B)
+
+
+
+
+
+
+
+
